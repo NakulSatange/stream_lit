4a0bce58cfba53f3b5382f6b5b2448db4acb33f7
NakulSatange <nakul27satange@gmail.com>
2024-08-12 08:15:04 +0530

    Update fashion_retailing_app.py

--- fashion_retailing_app.py
+++ fashion_retailing_app.py
@@ -2,42 +2,42 @@ import streamlit as st
 
 # Define product data with image paths
 products = {
-    "Men": [
-        {"name": "Men's Shirt", "price": 30, "image": "images/mens_shirt.jpg"},
-        {"name": "Men's Jeans", "price": 50, "image": "images/mens_jeans.jpg"},
-        {"name": "Men's Jacket", "price": 80, "image": "images/mens_jacket.jpg"},
-        {"name": "Men's Shoes", "price": 60, "image": "images/mens_shoes.jpg"},
-        {"name": "Men's Tie", "price": 25, "image": "images/mens_tie.jpg"},
-        {"name": "Men's Shorts", "price": 35, "image": "images/mens_shorts.jpg"},
-        {"name": "Men's Socks", "price": 10, "image": "images/mens_socks.jpg"},
-        {"name": "Men's Cap", "price": 20, "image": "images/mens_cap.jpg"},
-        {"name": "Men's Belt", "price": 40, "image": "images/mens_belt.jpg"},
-        {"name": "Men's Sweater", "price": 45, "image": "images/mens_sweater.jpg"}
+    "Men": [ 
+        {"name": "Leather Jacket", "price": "$120", "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQJDNGgIxiRmlygjdq5XjVX8QlP91EW3cFKETJSrEWS2oMunVQxiZSCCiR6ouwx"},
+        {"name": "Casual Shirt", "price": "$30", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjbYXQ63uJC3KQQQlhG2qIZ9tOCzKMsQGa-ROrjHqKXzjnF22s6sJKJfjIkLJw"},
+        {"name": "Running Shoes", "price": "$75", "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRO7ewirsejNF26IGHhuAPk2hmcOdQLzIPCTI6azswGwFeYyx0_GjoWdgffhsWv"},
+        {"name": "Jeans", "price": "$50", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRQfT-yLWHup2RT7gUPVK1uUwQKWcthuuEF4rjMEkGKNUVe3Rg0mstmRdNRggVS"},
+        {"name": "Wool Sweater", "price": "$60", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQB_eMql1tECrptezm5FMsTSpgDhuzpKqVvqxf64PHQHAPMxEXOF58O2jPTt_jl"},
+        {"name": "Shorts", "price": "$25", "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRyUX2a4LGMGDQJVcn5vivETIakrjQ_XFTFZ7YI2b5bTN_hCKRd4y34COLHwuBi"},
+        {"name": "Dress Shirt", "price": "$40", "image": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRRkuv00dDdL_pok91RrN5mAvWOHp2d6iPOLsDQlbuVEN3zPZHN31JhR_kN9_8-"},
+        {"name": "Blazer", "price": "$150", "image": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRVsdgW_9_zgx6yh1HM75ESblB7BTHZu_a1kec8lEThujl8uPa3cHW6BfGiE3ZP"},
+        {"name": "Boots", "price": "$85", "image": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS9dz-dR5aQ4L5Emc09f64t-4ej-chnFRn7xZNKifSzItNTC3GARYtnskKer1KL"},
+        {"name": "Sweatpants", "price": "$35", "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQrpvNyZ9d7VuXicUr6r_fGDGcfz7d_twBb76igjsnbNV4VxYM2wkWsBmIvj3qB"},
     ],
     "Women": [
-        {"name": "Women's Dress", "price": 60, "image": "images/womens_dress.jpg"},
-        {"name": "Women's Skirt", "price": 40, "image": "images/womens_skirt.jpg"},
-        {"name": "Women's Blouse", "price": 35, "image": "images/womens_blouse.jpg"},
-        {"name": "Women's Jacket", "price": 70, "image": "images/womens_jacket.jpg"},
-        {"name": "Women's Shoes", "price": 55, "image": "images/womens_shoes.jpg"},
-        {"name": "Women's Scarf", "price": 25, "image": "images/womens_scarf.jpg"},
-        {"name": "Women's Purse", "price": 80, "image": "images/womens_purse.jpg"},
-        {"name": "Women's Gloves", "price": 15, "image": "images/womens_gloves.jpg"},
-        {"name": "Women's Hat", "price": 30, "image": "images/womens_hat.jpg"},
-        {"name": "Women's Tights", "price": 20, "image": "images/womens_tights.jpg"}
+        {"name": "Summer Dress", "price": "$80", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSjc5vpJn_x3LJSo3U6nZ5LDVPJrdtZSVa6T9HZHwQJuR4ZCMDJShCuFs87uL8t"},
+        {"name": "High Heels", "price": "$60", "image": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS7f0Of9rzqnLhcDJP14gEuCvjP78Po6PiW_bYVL6cvORTuUcSkgS1GfK45jqaM"},
+        {"name": "Oversized Ts", "price": "$45", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSbfkfEJcB0uRdO_xHUtIPBVdN-QPgPtWpk5w9eaDhaXn2NkaqD4Vz8B7pqk2_"},
+        {"name": "Skirt", "price": "$35", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAsgmRdJ_pw_BIFeqrsYiPUaaYmtC-XczHGkX6Pyd2GS1V9QsU0FyX1hHnIEPr"},
+        {"name": "Cardigan", "price": "$50", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTIFhPaQo3NS2fCTm52rNSl2gHohBKLYboBO8-WB1GQsc9alnYAUnWNyefGWR3W"},
+        {"name": "Leggings", "price": "$40", "image": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTTI8JQT_Z5pCiuCqpBC3OUbgVxaCI_paCjn17El3MIbh-L_m6Pvlg8AcAkSzRG"},
+        {"name": "Coat", "price": "$120", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTswni0X21cNxL1zlG25nRsLyooAEGIRkYkd2ylvIUNOLzDtXdeFtGFD2xRnW5C"},
+        {"name": "Tank Top", "price": "$20", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQslUr3liFqMc1Uf__ZWS0omaGYptlp-dcu3_Onl_we3bePCsW0f6u10sOtrdGN"},
+        {"name": "Sweater Dress", "price": "$70", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQIgX9AnlSSoKDsE7MfI09YCUSwfA3C4rWjy0HeZWzHJ3M7qd0hkSh3f7nXREx-"},
+        {"name": "Chinos", "price": "$55", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi3hOQSb6JKwij_BtJ224SBCwexuPT6HWBLFnhR-vG1Zkp7XGF1E0N76ypZaQA"},
     ],
     "Kids": [
-        {"name": "Kids T-Shirt", "price": 20, "image": "images/kids_tshirt.jpg"},
-        {"name": "Kids Shorts", "price": 25, "image": "images/kids_shorts.jpg"},
-        {"name": "Kids Jacket", "price": 45, "image": "images/kids_jacket.jpg"},
-        {"name": "Kids Shoes", "price": 30, "image": "images/kids_shoes.jpg"},
-        {"name": "Kids Dress", "price": 40, "image": "images/kids_dress.jpg"},
-        {"name": "Kids Hat", "price": 15, "image": "images/kids_hat.jpg"},
-        {"name": "Kids Socks", "price": 10, "image": "images/kids_socks.jpg"},
-        {"name": "Kids Gloves", "price": 12, "image": "images/kids_gloves.jpg"},
-        {"name": "Kids Sweater", "price": 35, "image": "images/kids_sweater.jpg"},
-        {"name": "Kids Backpack", "price": 50, "image": "images/kids_backpack.jpg"}
-    ]
+        {"name": "T-Shirt", "price": "$15", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdyMcqxjR06z9RXBLPLQHlebdejrS58cWZhETp-DuoaF5gyjXKvHXbnNNQdzqt"},
+        {"name": "Jeans", "price": "$25", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDtb-nUnHg0FBhISoK1z98AGyKnprk-e2KXSIW3N9ZM9Yh-jj50Pqrjd_vzJIY"},
+        {"name": "Sweater", "price": "$20", "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRP2MKyd52vzTvOi_x9psrYkTeJ56Y-urARQKX4OwNstdfoFRJ47ildsFdpuTXm" },
+        {"name": "Jacket", "price": "$40", "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSyB1Nh_4GOHuQda8EMSnVDsQyiT1VXWA_pvqZVHg4fzJasoB8sSX72PeFxOuIb"},
+        {"name": "Shorts", "price": "$18", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSOK2gd7Ha7Y3IOaK5NdNhC0HzhosH5AAfaxNwMjQ1O1LRqGGXDq9gvXajhfKKo"},
+        {"name": "Sneakers", "price": "$30", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVVgatS5fKfIlpNT2mOiX1_LWLeOIZcftC6lNdpuH0wu1LhEBhTma5codcxLj1"},
+        {"name": "Dress", "price": "$25", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTS9g1kqKFomKko0gU9kw616E_1STTOsPqhJmGh4cwjBiIf3WO9H4YksyxUGVJM"},
+        {"name": "Co-ords", "price": "$28", "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCZQXHjo_vmUlFVBurV9lsXcvJY_KfRgLu3Ws1VtTJ2hLiQd35BnNq-N6HA3_P"},
+        {"name": "Cap", "price": "$10", "image": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRx0BCEiZ8eFVtRPWCi5jj4bc6-OzSYg0M1OVl7G-F-HXv1UkHrtTjdM128LLjF"},
+        {"name": "Raincoat", "price": "$35", "image": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTYrapxQN__8FrwnzEvfBoIt5E-Y8BAukhPvTHpfYmudQwKU8dPtjG43noCQ08L"},
+   ]
 }
 
 # Function to display products
